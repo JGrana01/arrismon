89ded543496587ee74b9e77f8c8fc5b089c93c63
WRKDBF-Guy <103457105+WRKDBF-Guy@users.noreply.github.com>
2022-04-13 16:21:29 -0700

    Update arrismon.sh

--- arrismon.sh
+++ arrismon.sh
@@ -49,7 +49,7 @@ readonly BOLD="\\e[1m"
 readonly SETTING="${BOLD}\\e[36m"
 readonly CLEARFORMAT="\\e[0m"
 ### End of output format variables ###
-
+	MainMenu
 # $1 = print to syslog, $2 = message to print, $3 = log level
 Print_Output(){
 	if [ "$1" = "true" ]; then
